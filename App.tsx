8a8966972e73cca892902e07c4253db88346cb79
Luiz Pires <lpiresjunior@gmail.com>
2023-11-13 20:30:40 -0300

    feat: add app theme

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 
-import {Text} from './src/ui/core/text';
+import {ThemeProvider} from '@shopify/restyle';
+
+import {Text, theme} from './src/ui';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge" italic>
-        Socials
-      </Text>
-      <Text preset="headingMedium">Socials</Text>
-      <Text preset="paragraphMedium">Socials</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge" italic>
+          Socials
+        </Text>
+        <Text preset="headingMedium">Socials</Text>
+        <Text preset="paragraphMedium">Socials</Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
