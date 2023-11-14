986854a7aae18213c33a75b582f5cd57cca4a28f
Luiz Pires <lpiresjunior@gmail.com>
2023-11-14 09:56:18 -0300

    feat: create core box

---
+++ src/ui/core/box.tsx
@@ -0,0 +1,39 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps as RNTouchableOpacityProps,
+} from 'react-native';
+
+import {
+  backgroundColor,
+  BackgroundColorProps,
+  border,
+  BorderProps,
+  createBox,
+  createRestyleComponent,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+
+import {ThemeProps} from '../theme';
+
+export const Box = createBox<ThemeProps>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<ThemeProps> &
+  SpacingProps<ThemeProps> &
+  LayoutProps<ThemeProps> &
+  BorderProps<ThemeProps> &
+  SpacingShorthandProps<ThemeProps> &
+  RNTouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  ThemeProps
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity,
+);
