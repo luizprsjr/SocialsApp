a5b272dea01a02fdeb7418e1c0c4a55733abd99b
Luiz Pires <lpiresjunior@gmail.com>
2023-11-14 11:46:02 -0300

    feat: create app theme hook

---
+++ src/hooks/use-app-theme.ts
@@ -0,0 +1,7 @@
+import {useTheme} from '@shopify/restyle';
+
+import {ThemeProps} from '../ui';
+
+export function useAppTheme() {
+  return useTheme<ThemeProps>();
+}
--- src/ui/core/activity-indicator.tsx
+++ src/ui/core/activity-indicator.tsx
@@ -4,16 +4,15 @@ import {
   ActivityIndicatorProps,
 } from 'react-native';
 
-import {useTheme} from '@shopify/restyle';
-
-import {ThemeColors, ThemeProps} from '../theme';
+import {useAppTheme} from '../../hooks/use-app-theme';
+import {ThemeColors} from '../theme';
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
   color: ThemeColors;
 }
 
 export function ActivityIndicator({color}: Props) {
-  const {colors} = useTheme<ThemeProps>();
+  const {colors} = useAppTheme();
 
   return <RNActivityIndicator color={colors[color]} />;
 }
