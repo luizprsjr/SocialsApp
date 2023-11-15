5f739c5af5a3e940160bd7ea84ae11365ba0f882
Luiz Pires <lpiresjunior@gmail.com>
2023-11-14 23:06:05 -0300

    refactor: add controlled inputs to sign up

--- src/screens/sign-up/sign-up-screen.tsx
+++ src/screens/sign-up/sign-up-screen.tsx
@@ -1,54 +1,109 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {useResetNavigationSuccess} from '../../hooks/use-reset-navigation';
 import {RootStackParamList} from '../../routes';
-import {Button, PasswordInput, Screen, Text, TextInput} from '../../ui';
+import {
+  Button,
+  ControlledPasswordInput,
+  ControlledTextInput,
+  Screen,
+  Text,
+} from '../../ui';
+
+type SignUpFormType = {
+  username: string;
+  fullName: string;
+  email: string;
+  password: string;
+};
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function SignUpScreen({navigation}: ScreenProps) {
   const {reset} = useResetNavigationSuccess();
-
-  function submitForm() {
-    reset({
-      title: 'Sua conta foi criada com sucesso!',
-      description: 'Agora é só fazer login na nossa plataforma',
-      icon: {
-        name: 'checkRound',
-        color: 'success',
-      },
-    });
+  const {control, formState, handleSubmit} = useForm<SignUpFormType>({
+    defaultValues: {
+      username: '',
+      fullName: '',
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+  function submitForm(formValues: SignUpFormType) {
+    console.log(formValues);
+    // reset({
+    //   title: 'Sua conta foi criada com sucesso!',
+    //   description: 'Agora é só fazer login na nossa plataforma',
+    //   icon: {
+    //     name: 'checkRound',
+    //     color: 'success',
+    //   },
+    // });
   }
-
   return (
     <Screen canGoBack scrollable>
       <Text preset="headingLarge" mb="s32">
         Criar uma conta
       </Text>
 
-      <TextInput label="Seu username" placeholder="@" boxProps={{mb: 's20'}} />
-      <TextInput
+      <ControlledTextInput
+        control={control}
+        name="username"
+        rules={{required: 'Username obrigatório'}}
+        label="Seu username"
+        placeholder="@"
+        boxProps={{mb: 's20'}}
+      />
+
+      <ControlledTextInput
+        control={control}
+        name="fullName"
+        rules={{required: 'Nome obrigatório'}}
+        autoCapitalize="words"
         label="Nome Completo"
         placeholder="Digite seu nome completo"
         boxProps={{mb: 's20'}}
       />
-      <TextInput
+      <ControlledTextInput
+        control={control}
+        name="email"
+        rules={{
+          required: 'E-mail obrigatório',
+          pattern: {
+            value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            message: 'E-mail inválido',
+          },
+        }}
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{mb: 's20'}}
       />
 
-      <PasswordInput
+      <ControlledPasswordInput
+        control={control}
+        name="password"
+        rules={{
+          required: 'Senha obrigatória',
+          minLength: {
+            value: 8,
+            message: 'Senha deve ter no mínimo 8 caracteres',
+          },
+        }}
         label="Senha"
-        secureTextEntry
         placeholder="Digite sua senha"
         boxProps={{mb: 's48'}}
       />
 
-      <Button onPress={submitForm} title="Criar uma conta" />
+      <Button
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+        title="Criar uma conta"
+      />
     </Screen>
   );
 }
