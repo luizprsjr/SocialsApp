8808ded26f15b4eb46860e7c87d709efc6bcb116
Luiz Pires <lpiresjunior@gmail.com>
2023-11-14 23:28:46 -0300

    refactor: use zod to handle with form state

--- src/screens/sign-up/sign-up-screen.tsx
+++ src/screens/sign-up/sign-up-screen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useForm} from 'react-hook-form';
 
+import {zodResolver} from '@hookform/resolvers/zod';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {useResetNavigationSuccess} from '../../hooks/use-reset-navigation';
@@ -12,20 +13,15 @@ import {
   Screen,
   Text,
 } from '../../ui';
-
-type SignUpFormType = {
-  username: string;
-  fullName: string;
-  email: string;
-  password: string;
-};
+import {SignUpSchema, signUpSchema} from './sign-up-schema';
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function SignUpScreen({navigation}: ScreenProps) {
   const {reset} = useResetNavigationSuccess();
-  const {control, formState, handleSubmit} = useForm<SignUpFormType>({
+  const {control, formState, handleSubmit} = useForm<SignUpSchema>({
+    resolver: zodResolver(signUpSchema),
     defaultValues: {
       username: '',
       fullName: '',
@@ -34,7 +30,8 @@ export function SignUpScreen({navigation}: ScreenProps) {
     },
     mode: 'onChange',
   });
-  function submitForm(formValues: SignUpFormType) {
+
+  function submitForm(formValues: SignUpSchema) {
     console.log(formValues);
     // reset({
     //   title: 'Sua conta foi criada com sucesso!',
@@ -45,6 +42,7 @@ export function SignUpScreen({navigation}: ScreenProps) {
     //   },
     // });
   }
+
   return (
     <Screen canGoBack scrollable>
       <Text preset="headingLarge" mb="s32">
@@ -54,7 +52,6 @@ export function SignUpScreen({navigation}: ScreenProps) {
       <ControlledTextInput
         control={control}
         name="username"
-        rules={{required: 'Username obrigatório'}}
         label="Seu username"
         placeholder="@"
         boxProps={{mb: 's20'}}
@@ -63,22 +60,15 @@ export function SignUpScreen({navigation}: ScreenProps) {
       <ControlledTextInput
         control={control}
         name="fullName"
-        rules={{required: 'Nome obrigatório'}}
         autoCapitalize="words"
         label="Nome Completo"
         placeholder="Digite seu nome completo"
         boxProps={{mb: 's20'}}
       />
+
       <ControlledTextInput
         control={control}
         name="email"
-        rules={{
-          required: 'E-mail obrigatório',
-          pattern: {
-            value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
-            message: 'E-mail inválido',
-          },
-        }}
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{mb: 's20'}}
@@ -87,13 +77,6 @@ export function SignUpScreen({navigation}: ScreenProps) {
       <ControlledPasswordInput
         control={control}
         name="password"
-        rules={{
-          required: 'Senha obrigatória',
-          minLength: {
-            value: 8,
-            message: 'Senha deve ter no mínimo 8 caracteres',
-          },
-        }}
         label="Senha"
         placeholder="Digite sua senha"
         boxProps={{mb: 's48'}}
