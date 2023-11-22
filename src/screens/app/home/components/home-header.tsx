08ceba50dee0b955dbd0a9c492cce21b7da664f3
Luiz Pires <lpiresjunior@gmail.com>
2023-11-21 21:43:04 -0300

    feat: add app logo

--- src/screens/app/home/components/home-header.tsx
+++ src/screens/app/home/components/home-header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import {useAppSafeArea} from '@hooks';
-import {Box, BoxProps, Icon} from '@ui';
+import {Box, BoxProps, Icon, Logo} from '@ui';
 
 export function HomeHeader() {
   const {top} = useAppSafeArea();
 
   return (
     <Box {...$wrapper} style={{paddingTop: top}}>
-      <Box backgroundColor="carrotSecondary" height={16} width={70} />
+      <Logo width={70} />
       <Box flexDirection="row">
         <Box mr="s24">
           <Icon name="search" />
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -2,3 +2,4 @@ export * from './core';
 export * from './components';
 export * from './theme';
 export * from './icons';
+export * from './brand';
