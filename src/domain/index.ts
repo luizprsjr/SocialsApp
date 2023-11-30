328f9c2bd050d3b1c4b5f2501a8a1f57efdfcdbf
Luiz Pires <lpiresjunior@gmail.com>
2023-11-30 19:10:01 -0300

    feat: implement is allow to delete post comment

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,3 +2,4 @@ export * from './post/post-types';
 export * from './post/post-service';
 export * from './post/use-cases/use-post-list';
 export * from './post-comment';
+export * from './auth';
--- src/screens/app/post-comment/post-comment-screen.tsx
+++ src/screens/app/post-comment/post-comment-screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FlatList, ListRenderItemInfo} from 'react-native';
 
-import {PostComment, usePostCommentList} from '@domain';
+import {PostComment, usePostCommentList, useUser} from '@domain';
 import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
 import {Box, Screen} from '@ui';
@@ -15,13 +15,21 @@ import {
 export function PostCommentScreen({
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
-  const postId = route.params.postId;
+  const {postId, postAuthorId} = route.params;
   const {list, fetchNextPage, hasNextPage, refresh} =
     usePostCommentList(postId);
+  const {id} = useUser();
   const {bottom} = useAppSafeArea();
 
   function renderItem({item}: ListRenderItemInfo<PostComment>) {
-    return <PostCommentItem postComment={item} />;
+    return (
+      <PostCommentItem
+        postComment={item}
+        onRemoveComment={refresh}
+        userId={id}
+        postAuthorId={postAuthorId}
+      />
+    );
   }
 
   return (
