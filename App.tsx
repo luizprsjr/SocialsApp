3f4dcf339b6cbe5864b4ae7898003ab254268340
Luiz Pires <lpiresjunior@gmail.com>
2023-12-01 08:36:36 -0300

    chore: add react query

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Router} from '@routes';
 import {Toast, theme} from '@ui';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <Router />
-        <Toast />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          <Router />
+          <Toast />
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
