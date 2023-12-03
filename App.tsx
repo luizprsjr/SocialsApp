0e2a6f6f458a63ab495f20f8f95979e3ddadd81a
Luiz Pires <lpiresjunior@gmail.com>
2023-12-03 20:28:24 -0300

    feat: create auth credentials context

--- App.tsx
+++ App.tsx
@@ -5,20 +5,23 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Router} from '@routes';
+import {AuthCredentialsProvider} from '@services';
 import {Toast, theme} from '@ui';
 
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={theme}>
-          <Router />
-          <Toast />
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <AuthCredentialsProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={theme}>
+            <Router />
+            <Toast />
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </AuthCredentialsProvider>
   );
 }
 
--- src/services/auth-credentials/index.ts
+++ src/services/auth-credentials/index.ts
@@ -1,2 +1,3 @@
 export * from './auth-credentials-type';
 export * from './use-auth-credentials';
+export {AuthCredentialsProvider} from './providers/auth-credentials-provider';
