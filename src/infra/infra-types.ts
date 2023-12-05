b19a16dcea81ff5aac276ad117c2b7cc8eab36ac
Luiz Pires <lpiresjunior@gmail.com>
2023-12-04 23:25:43 -0300

    feat: add auth email available to hook

--- src/infra/infra-types.ts
+++ src/infra/infra-types.ts
@@ -3,4 +3,5 @@ export enum QueryKeys {
   PostCommentList = 'PostCommentList',
   UserGetById = 'UserGetById',
   IsUserNameAvailable = 'IsUserNameAvailable',
+  IsEmailAvailable = 'IsEmailAvailable',
 }
