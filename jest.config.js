83d70d83f9be1d9fee8037f69851081eb52caf03
Luiz Pires <lpiresjunior@gmail.com>
2023-12-05 04:08:47 -0300

    chore: config jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverageFrom: [
+    'src/{ui/core,ui/components,utils}/**/*.{js,jsx,ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
 };
