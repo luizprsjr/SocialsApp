7867de7288c9cd4e94bf1942783b3d3914ed9979
Luiz Pires <lpiresjunior@gmail.com>
2023-12-06 00:45:04 -0300

    test: setup QueryClientProvider

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,15 +2,31 @@ import React, {ReactElement} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RenderOptions, render} from '@testing-library/react-native';
 
 import {theme} from '@ui';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      gcTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      gcTime: Infinity,
+    },
+  },
+});
+
 export const AllTheProviders = ({children}: {children: React.ReactNode}) => {
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>{children}</NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
