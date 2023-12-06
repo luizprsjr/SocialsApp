6cb523c88de0f6879fac13909e7af017e388cbd7
Luiz Pires <lpiresjunior@gmail.com>
2023-12-05 21:35:34 -0300

    test: ensure password input show the password if button is pressed

--- src/ui/core/password-input/__test__/password-input.test.tsx
+++ src/ui/core/password-input/__test__/password-input.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import {render, screen} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
-import {PasswordInput} from '../password-input';
+import {IconProps, PasswordInput} from '@ui';
 
 describe('<PasswordInput />', () => {
   it('should start with hidden password', () => {
@@ -17,5 +17,24 @@ describe('<PasswordInput />', () => {
     );
     const inputElement = screen.getByPlaceholderText(/password/i);
     expect(inputElement).toHaveProp('secureTextEntry', true);
+  });
+
+  it('should show the password when pressing the eye button', () => {
+    const mockedOnChange = jest.fn();
+    render(
+      <PasswordInput
+        label="Password"
+        placeholder="password"
+        value="123456"
+        onChangeText={mockedOnChange}
+      />,
+    );
+    const eyeIcon: IconProps['name'] = 'eyeOff';
+    fireEvent.press(screen.getByTestId(eyeIcon));
+    const eyeOffIcon: IconProps['name'] = 'eyeOn';
+    const eyeOffIconElement = screen.getByTestId(eyeOffIcon);
+    expect(eyeOffIconElement).toBeTruthy();
+    const inputElement = screen.getByPlaceholderText(/password/);
+    expect(inputElement).toHaveProp('secureTextEntry', false);
   });
 });
