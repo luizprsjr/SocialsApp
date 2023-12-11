c6eb9fd4ebbb6ff5cf5ed2d6a26bd43e1f7495a8
Luiz Pires <lpiresjunior@gmail.com>
2023-12-11 00:59:07 -0300

    feat: create use user search hook

--- src/domain/user/index.ts
+++ src/domain/user/index.ts
@@ -2,3 +2,4 @@ export * from './user-types';
 export * from './user-service';
 export * from './user-adapter';
 export * from './use-cases/use-user-get-by-id';
+export * from './use-cases/use-user-search';
--- src/screens/app/search/search-screen.tsx
+++ src/screens/app/search/search-screen.tsx
@@ -1,21 +1,29 @@
 import React, {useState} from 'react';
 
+import {useUserSearch} from '@domain';
+import {useDebounce} from '@hooks';
 import {AppScreenProps} from '@routes';
 import {Icon, Screen, Text, TextInput} from '@ui';
 
 export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
   const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search);
+  const {list} = useUserSearch(debouncedSearch);
+
   return (
     <Screen
       canGoBack
       HeaderComponent={
         <TextInput
+          placeholder="Digite sua busca"
           value={search}
           onChangeText={setSearch}
           LeftComponent={<Icon color="gray3" name="search" />}
         />
       }>
-      <Text>Search Screen</Text>
+      {list.map(user => (
+        <Text key={user.id}>{user.username}</Text>
+      ))}
     </Screen>
   );
 }
