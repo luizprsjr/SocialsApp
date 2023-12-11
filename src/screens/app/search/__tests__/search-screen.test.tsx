d2b3900591c1a8b89f9802f1656282f4e47b3428
Luiz Pires <lpiresjunior@gmail.com>
2023-12-11 04:25:09 -0300

    test: ensure search screen search the users

--- src/screens/app/search/__tests__/search-screen.test.tsx
+++ src/screens/app/search/__tests__/search-screen.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {mockUtils} from '@test';
+import {mockUtils, server, userMocked} from '@test';
 import {fireEvent, renderScreen, screen} from 'test-utils';
 
 import {AppStack} from '@routes';
@@ -8,15 +8,26 @@ import {authCredentialsStorage} from '@services';
 
 describe('integration: SearchScreen', () => {
   beforeAll(() => {
+    server.listen();
     jest
       .spyOn(authCredentialsStorage, 'get')
       .mockResolvedValue(mockUtils.mateusAuthCredentials);
   });
-  it('search flow', () => {
+  afterEach(() => {
+    server.resetHandlers();
+  });
+  afterAll(() => {
+    server.close();
+    jest.resetAllMocks();
+  });
+  it('should search the users', async () => {
     renderScreen(<AppStack initialRouteName="SearchScreen" />);
 
     const inputText = screen.getByPlaceholderText(/digite sua busca/i);
-    expect(inputText).toBeOnTheScreen();
     fireEvent.changeText(inputText, 'mar');
+    const user1 = await screen.findByText(userMocked.user1.username);
+    expect(user1).toBeOnTheScreen();
+    const user2 = await screen.findByText(userMocked.user2.username);
+    expect(user2).toBeOnTheScreen();
   });
 });
--- src/test/server/server.ts
+++ src/test/server/server.ts
@@ -1,9 +1,12 @@
 import {setupServer} from 'msw/node';
 
 import {postCommentHandlers} from './post-comment/post-comment-handlers';
+import {userHandlers} from './user/user-handlers';
 
-export const server = setupServer(...postCommentHandlers);
+export const server = setupServer(...postCommentHandlers, ...userHandlers);
 
 export {mockedData as mockedPostComment} from './post-comment/mocks';
+
+export {userMocked} from './user/user-mocked';
 
 export {resetInMemoryResponse} from './post-comment/post-comment-handlers';
